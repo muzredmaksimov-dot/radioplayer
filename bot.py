961b712549e7a7bdaf75a2c10e41b0aa3ba959d7
mir-bot-tracks <muzredmaksimov@gmail.com>
2026-01-05 16:29:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -130,7 +130,7 @@ if __name__ == "__main__":
     if "RENDER" in os.environ:
         bot.remove_webhook()
         time.sleep(1)
-        bot.set_webhook(url=f"https://{os.environ.get('RENDER_EXTERNAL_HOSTNAME')}/webhook/{TOKEN}")
+        bot.set_webhook(url=f"https://radioplayer-tq0i.onrender.com/webhook/{TOKEN}")
         port = int(os.environ.get("PORT", 10000))
         app.run(host="0.0.0.0", port=port)
     else:
