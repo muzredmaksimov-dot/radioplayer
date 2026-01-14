97506b526b5ceb6b7b4e63669ebf023913e0d704
mir-bot-tracks <muzredmaksimov@gmail.com>
2026-01-14 18:39:51 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ GITHUB_REPO = "muzredmaksimov-dot/radioplayer"
 GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
 
 CHANNEL_ID = -1002905716039
-CHANNEL_URL = "https://t.me/test111"
+CHANNEL_URL = "https://t.me/testposring"
 
 bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
